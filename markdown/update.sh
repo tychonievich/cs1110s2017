ad61457a5d5cad5016915330e43b8357c4c53248
Luther Tychonievich <lat7h@virginia.edu>
2017-01-09 12:18:19 -0500

    autocommit caused by update

--- markdown/update.sh
+++ markdown/update.sh
@@ -5,7 +5,7 @@ git commit -a -m 'autocommit caused by update'
 git push
 
 here="$(dirname "$(readlink -m "$0")")/"
-target="$HOME/public_html/1110/S2017/"
+target="$(readlink -f "$HOME/public_html/1110/S2017/")"
 remote=cs1110@stardock.cs.virginia.edu/home/cs1110/www/
 if [ ! -d "$target" ]
 then
