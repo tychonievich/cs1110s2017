4a3cdeb0b313666ef257292317509ed119c4b76c
Luther Tychonievich <tychonievich@gmail.com>
2017-01-20 12:11:02 -0500

    autocommit caused by update

--- roster.py
+++ roster.py
@@ -8,13 +8,13 @@ def dataurl(fname):
         ans += b64encode(stream.read()).decode('ascii')
     return ans
 
-def sql(record, f):
-    print("INSERT OR IGNORE INTO person (compid, name, image, role, section, help_time, last_helped) VALUES ('{}', '{}', '{}', '{}', '{}', 0, 0);".format(
+def sql(record, f, img, grp):
+    print("INSERT OR IGNORE INTO person (compid, name, image, role, section, help_time, last_helped) VALUES ('{}', '{}', '{}', '{}', {}, 0, 0);".format(
             record['User ID'].replace("'", "''"),
             record['Name'].replace("'", "''"),
-            record['img'].replace("'", "''"),
+            img.replace("'", "''"),
             'Student' if 'tud' in record['Role'] else 'Staff',
-            grp.replace("'", "''"),
+            "'"+grp.replace("'", "''")+"'" if grp is not None else 'null',
         ), file=f)
 def csv(record, f):
     print('"'+'","'.join([
@@ -25,50 +25,42 @@ def csv(record, f):
     ])+'"', file=f) 
 
 
-with open('roster.csv', 'w') as csv:
-    with open('roster.sql', 'w') as sql:
-
-fname = sorted(glob('*.xls'))[-1]
-records = pyexcel.get_sheet(file_name=fname, name_columns_by_row=1, sheet_name='Groups')
-done = set(['','User ID'])
-for record in records.to_records():
-    try:
-        if record['User ID'] in done: continue
-        else: done.add(record['User ID'])
+with open('roster.csv', 'w') as fcsv:
+    with open('roster.sql', 'w') as fsql:
         
-        if 'Groups' not in record or len(record['Groups']) == 0:
-            grp = 'staff'
-        else:
-            grp = sorted(record['Groups'].split(', '))[-1].split(' ')[2]
-            if grp[3] == '0': grp = grp[5:]
-            else: grp = grp[:4]
-        img = glob('/home/lat7h/Students/'+record['User ID']+'.jpg')+glob('/home/lat7h/Students/'+record['User ID']+'@*.jpg')+['/home/lat7h/Students/empty.png']
-        img=dataurl(img[0])
-        print("INSERT OR IGNORE INTO person (compid, name, image, role, section, help_time, last_helped) VALUES ('{}', '{}', '{}', '{}', '{}', 0, 0);".format(
-            record['User ID'].replace("'", "''"),
-            record['Name'].replace("'", "''"),
-            img.replace("'", "''"),
-            'Student' if 'tud' in record['Role'] else 'Staff',
-            grp.replace("'", "''"),
-        ))
-    except IndexError:
-        pass
+
+        fname = sorted(glob('*.xls'))[-1]
+        records = pyexcel.get_sheet(file_name=fname, name_columns_by_row=1, sheet_name='Groups')
+        done = set(['','User ID'])
+        for record in records.to_records():
+            try:
+                if record['User ID'] in done: continue
+                else: done.add(record['User ID'])
+                
+                if 'Groups' not in record or len(record['Groups']) == 0:
+                    grp = 'staff'
+                else:
+                    grp = sorted(record['Groups'].split(', '))[-1].split(' ')[2]
+                    if grp[3] == '0': grp = grp[5:]
+                    else: grp = grp[:4]
+                img = glob('/home/lat7h/Students/'+record['User ID']+'.jpg')+glob('/home/lat7h/Students/'+record['User ID']+'@*.jpg')+['/home/lat7h/Students/empty.png']
+                img=dataurl(img[0])
+                sql(record, fsql, img, grp)
+                csv(recrod, fcsv)
+            except IndexError:
+                pass
 
 
-records = pyexcel.get_sheet(file_name=fname, name_columns_by_row=1, sheet_name='Roster')
-for record in records.to_records():
-    try:
-        if record['User ID'] in done: continue
-        else: done.add(record['User ID'])
-        
-        img = glob('/home/lat7h/Students/'+record['User ID']+'.jpg')+glob('/home/lat7h/Students/'+record['User ID']+'@*.jpg')+['/home/lat7h/Students/empty.png']
-        img=dataurl(img[0])
-        print("INSERT OR IGNORE INTO person (compid, name, image, role, section, help_time, last_helped) VALUES ('{}', '{}', '{}', '{}', null, 0, 0);".format(
-            record['User ID'].replace("'", "''"),
-            record['Name'].replace("'", "''"),
-            img.replace("'", "''"),
-            'Student' if 'tud' in record['Role'] else 'Staff',
-            grp.replace("'", "''"),
-        ))
-    except IndexError:
-        pass
+        records = pyexcel.get_sheet(file_name=fname, name_columns_by_row=1, sheet_name='Roster')
+        for record in records.to_records():
+            try:
+                if record['User ID'] in done: continue
+                else: done.add(record['User ID'])
+                record['Groups'] = ''
+                
+                img = glob('/home/lat7h/Students/'+record['User ID']+'.jpg')+glob('/home/lat7h/Students/'+record['User ID']+'@*.jpg')+['/home/lat7h/Students/empty.png']
+                img=dataurl(img[0])
+                sql(record, fsql, img, None)
+                csv(record, fcsv)
+            except IndexError:
+                pass
