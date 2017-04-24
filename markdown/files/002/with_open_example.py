f8fe652719795ebad621dbda70fc2dc8c1f0d0ec
upsornjp <upsornjp@users.noreply.github.com>
2017-04-24 12:20:42 -0400

    Add files via upload

--- markdown/files/002/with_open_example.py
+++ markdown/files/002/with_open_example.py
@@ -4,7 +4,7 @@
 def write_file():
     outfile = open('cartoon.txt', 'w')
     for i in range(3):
-        user_input = input("What is your favorite cartoon characters? ")
+        user_input = input("What is your favorite cartoon character? ")
         outfile.write(user_input + '\n')    # must write string with write()
     outfile.close()
 
@@ -12,7 +12,7 @@ def write_file():
 def with_open():
     # to show that the file is opened and closed without an explicit .close()
     for i in range(3):
-        user_input = input("What is your favorite cartoon characters? ")
+        user_input = input("What is your favorite cartoon character? ")
         # for each iteration, after entering the user_input, observe what is in the file
         with open('cartoon2.txt', 'w') as myoutfile:
             myoutfile.write(user_input + '\n')  # must write string with write()
@@ -21,7 +21,7 @@ def with_open():
     # the following code will produce the same output file as write_file() function
     # with open('cartoon2.txt', 'w') as myoutfile:
     #     for i in range(3):
-    #         user_input = input("What is your favorite cartoon characters? ")
+    #         user_input = input("What is your favorite cartoon character? ")
     #         myoutfile.write(user_input + '\n')    # must write string with write()
 
 
