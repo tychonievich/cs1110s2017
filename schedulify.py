4d3f71c7af24323d7b76756550148358d1bae651
Luther Tychonievich <tychonievich@gmail.com>
2017-05-05 15:21:01 -0400

    autocommit caused by update

--- schedulify.py
+++ schedulify.py
@@ -231,9 +231,18 @@ print('''
 <hr/>
 
 Per <a href="http://www.virginia.edu/registrar/exams.html#1172">the registrar</a>, all sections of 1110 and 1111 will have their final exam at 7--10 pm on Thursday, 11 May 2017.
-1110-001 will be in Gilmner 130; 1110-002 in Phys 203; 1111-001 in Rice 130.
-Conflicts with that time will be resolved the following day (Friday 12 May) at 10 am.
-No permission to take the exam earlier than 11 May or from off of UVa grounds will be granted.
+
+1110-001
+:   Gilmner 130
+
+1110-002
+:   Phys 203
+
+1111-001
+:   in Rice 130
+
+Conflicts with that time will be resolved the following day (Friday 12 May) at 10 am, location sent via email.
+No permission to take the exam earlier than 11 May or from off of UVA grounds will be granted without Deans' office request.
 
 <!-- You may report conflicts and request accomodations at <https://goo.gl/forms/hC5oRY2zJoVfQz023>. -->
 
